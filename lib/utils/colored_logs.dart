aee8085ff47332c8b1f44003e535a6e6a533ad9b
Kripa Jethwani <kripa@atliq.com>
2025-03-25 12:52:02 +0530

    Added: Support for colored logs in iOS

--- lib/utils/colored_logs.dart
+++ lib/utils/colored_logs.dart
@@ -1,27 +1,61 @@
+import 'dart:io';
+
 import 'package:flutter/foundation.dart';
 
 class ColoredLogs{
   static void error(String message){
     if(kDebugMode) {
+      if(Platform.isIOS){
+        message = '❌ $message';
+        print(message);
+        print('');
+        return;
+      }
       print('\x1B[31m$message\x1B[0m');
+      print('');
+      debugPrint = (String? message, {int? wrapWidth}) => '';
     }
   }
 
   static void info(String message){
     if(kDebugMode) {
+      if(Platform.isIOS){
+        message = '🔵 $message';
+        print(message);
+        print('');
+        return;
+      }
       print('\x1B[34m$message\x1B[0m');
+      print('');
+      debugPrint = (String? message, {int? wrapWidth}) => '';
     }
   }
 
   static void success(String message){
     if(kDebugMode) {
+      if(Platform.isIOS){
+        message = '✅ $message';
+        print(message);
+        print('');
+        return;
+      }
       print('\x1B[32m$message\x1B[0m');
+      print('');
+      debugPrint = (String? message, {int? wrapWidth}) => '';
     }
   }
 
   static void debug(String message){
     if(kDebugMode) {
+      if(Platform.isIOS){
+        message = '🟡 $message';
+        print(message);
+        print('');
+        return;
+      }
       print('\x1B[33m$message\x1B[0m');
+      print('');
+      debugPrint = (String? message, {int? wrapWidth}) => '';
     }
   }
 }
